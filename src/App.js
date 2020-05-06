7addfa9a253bfef53c199026439532932bb1f743
ravsharma6 <ravi.sharma1@nomura.com>
2020-05-06 13:31:02 +0530

    Code Change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My First Application Deployed in AWS
         </a>
       </header>
     </div>
